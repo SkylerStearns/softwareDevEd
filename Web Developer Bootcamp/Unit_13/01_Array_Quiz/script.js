31c5976f2632d4ca430f7e9958095f435ccb5f03
Skyler Stearns <skylerastearns@gmail.com>
2020-03-29 13:53:37 -0700

    Unit_12 * Unit 13 ** Created directory for project files ** Added 01_Array_Quiz directory and project files * Updated Table of Contents * Created 'dark mode' styling to global.css

---
+++ Web Developer Bootcamp/Unit_13/01_Array_Quiz/script.js
@@ -0,0 +1,17 @@
+// Exercise 1
+let numbers = [22, 67, 33, 96, 88];
+
+// What does the following line print?
+console.log(numbers[numbers.length]);
+
+
+
+// Exercise 2
+let friendGroups = [
+  ['Harry', 'Ron', 'Hermione'],
+  ['Malfoy', 'Crabba', 'Coyle'],
+  ['Mooney', Wormtail, 'Frongs']
+];
+
+// What is the result of this line?
+console.log(friendGroups[2][0]);
