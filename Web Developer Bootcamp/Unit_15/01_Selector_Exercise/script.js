cb323bbb5b4312b94be10d159d9633f28412750a
Skyler Stearns <skylerastearns@gmail.com>
2020-04-15 00:01:33 -0700

    Unit_14 * Completed Exercise 1: Selector Exercise * Updated Table of Contents

---
+++ Web Developer Bootcamp/Unit_15/01_Selector_Exercise/script.js
@@ -0,0 +1,23 @@
+// document.getElementById('first'));
+let answer1 = `document.getElementById('first')`;
+
+// document.getElementsByClassName('special')[0];
+let answer2 = `document.getElementsByClassName('special')[0]`;
+
+// document.getElementsByTagName('p')[0];
+let answer3 = `document.getElementsByTagName('p')[0]`;
+
+// document.querySelector('#first');
+// document.querySelectorAll('.special');
+// document.querySelectorAll('p');
+let answer4 = `document.querySelector('#first')`;
+let answer5 = `document.querySelectorAll('.special')`;
+let answer6 = `document.querySelectorAll('p')`;
+
+// document.querySelectorAll('#first')[0];
+// document.querySelectorAll('.special')[0];
+// document.querySelectorAll('p')[0];
+let answer7 = `document.querySelectorAll('#first')[0]`;
+let answer8 = `document.querySelectorAll('.special')[0]`;
+let answer9 = `document.querySelectorAll('p')[0]`;
+
